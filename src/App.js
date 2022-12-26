41cc02fca7edfd3007b9b91792a896052fe40467
Ivan Hu <ivanhu2246@gmail.com>
2022-12-26 20:47:42 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import meals from './db/meals.json';
 const App = () => {
   //const today = days.find(d => d.date === new Date().toISOString().split('T')[0]);
 
-  return JSON.stringify(days);
+  //return JSON.stringify(days);
+
+  return "deploy from master test"
 }
 
 export default App;
