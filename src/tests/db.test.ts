9144b4b7deec4b535c3150c4f4881abb9674f723
Ivan Hu <ivanhu2246@gmail.com>
2022-12-29 16:00:40 +1100

    Add tests for days.json

--- src/tests/db.test.ts
+++ src/tests/db.test.ts
@@ -1,5 +1,6 @@
-import { Ingredient, Dish } from '../interfaces'
+import { Ingredient, Dish, Day } from '../interfaces'
 import { expect, test } from '@jest/globals';
+import differenceInDays from 'date-fns/differenceInDays'
 
 test('dishes.json all id correspond to index', () => {
   const dishes: Dish[] = require('../db/dishes.json');
@@ -12,5 +13,12 @@ test('ingredients.json all id correspond to index', () => {
   const ingredients: Ingredient[] = require('../db/ingredients.json');
   ingredients.forEach((ingredient, i) => {
     expect(ingredient.id).toEqual(i);
+  })
+});
+
+test('days.json all dates sequentially correspond to index', () => {
+  const days: Day[] = require('../db/days.json');
+  days.forEach((day, i) => {
+    expect(differenceInDays(new Date(day.date), new Date(days[0].date))).toEqual(i);
   })
 });
